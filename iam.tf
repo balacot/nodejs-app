045adec7f0b31d6ac1d51584a7cf29d07a139133
balacot <53100531+balacot@users.noreply.github.com>
2025-08-06 16:31:01 -0500

    probando pipeline cambios3

--- iam.tf
+++ iam.tf
@@ -18,6 +18,26 @@ resource "aws_iam_role_policy_attachment" "codebuild_policies" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
 
+resource "aws_iam_role_policy" "codebuild_logs" {
+  name = "${var.project_name}-codebuild-logs-policy"
+  role = aws_iam_role.codebuild_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role" "codepipeline_role" {
   name = "${var.project_name}-codepipeline-role"
 
